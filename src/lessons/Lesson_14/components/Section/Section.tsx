d1106d43f40b4f1dc8599272e99ef14e42ccbb50
Brauer Sergej <bmw325d@web.de>
2026-03-25 16:06:19 +0100

    add employee project

--- src/lessons/Lesson_14/components/Section/Section.tsx
+++ src/lessons/Lesson_14/components/Section/Section.tsx
@@ -2,10 +2,17 @@ import Content from "lessons/Lesson_14/components/Content/Content";
 
 import { SectionTitle, SectionWrapper } from "./styles";
 
+// ! Props Drilling
+//   Section в props ловит её
+// * function Section ({userData}: any)
+
 function Section() {
   return (
     <SectionWrapper>
       <SectionTitle>Section Component</SectionTitle>
+      {/* Props Drilling */}
+      {/* ! Далее когда он её ловит мы можем передать её в <Content/> */}
+      {/* <Content userData={userData}/> */}
       <Content />
     </SectionWrapper>
   );
